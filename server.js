3d819df3f28d60a4e7314918743970faead90d8c
SlavaRaz <slavarazaranov87@gmail.com>
2024-10-29 13:21:02 +0200

    Add bug service

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 
 const app = express()
-app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+app.get('/api/bug', (req, res) =>
+    res.send('Hello there '))
+app.listen(3030, () =>
+    console.log('Server ready at port http://127.0.0.1:3030'))
