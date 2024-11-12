dc2ac243a1def38fca7b09b3e9151e17695deb9e
SlavaRaz <slavarazaranov87@gmail.com>
2024-11-12 22:55:55 +0200

    Add user support for backend

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import { bugService } from './services/bug.service.js'
 import { loggerService } from './services/logger.service.js'
-import cookieParser from 'cookie-parser'
+import { userService } from './services/user.service.js'
 
 
 const app = express()
@@ -15,7 +16,7 @@ app.get('/api/bug', (req, res) => {
 
         txt: req.query.txt || '',
         severity: req.query.severity || 0,
-        pageIdx: req.query.pageIdx ,
+        pageIdx: req.query.pageIdx,
         labels: req.query.labels ? req.query.labels.split(',') : '',
         sortBy: req.query.sortBy || 'title',
         sortDir: req.query.sortDir || 1,
@@ -86,6 +87,63 @@ app.delete('/api/bug/:bugId', (req, res) => {
             loggerService.error('Cannot remove bug', err)
             res.status(400).send('Cannot remove bug')
         })
+})
+
+// User API
+app.get('/api/user', (req, res) => {
+    userService.query()
+        .then(users => res.send(users))
+        .catch(err => {
+            loggerService.error('Cannot load users', err)
+            res.status(400).send('Cannot load users')
+        })
+})
+
+app.get('/api/user/:userId', (req, res) => {
+    const { userId } = req.params
+
+    userService.getById(userId)
+        .then(user => res.send(user))
+        .catch(err => {
+            loggerService.error('Cannot load user', err)
+            res.status(400).send('Cannot load user')
+        })
+})
+
+// Auth API
+app.post('/api/auth/login', (req, res) => {
+    const credentials = req.body
+
+    userService.checkLogin(credentials)
+        .then(user => {
+            if (user) {
+                const loginToken = userService.getLoginToken(user)
+                res.cookie('loginToken', loginToken)
+                res.send(user)
+            } else {
+                res.status(404).send('Invalid Credentials')
+            }
+        })
+})
+
+app.post('/api/auth/signup', (req, res) => {
+    const credentials = req.body
+
+    userService.save(credentials)
+        .then(user => {
+            if (user) {
+                const loginToken = userService.getLoginToken(user)
+                res.cookie('loginToken', loginToken)
+                res.send(user)
+            } else {
+                res.status(400).send('Cannot signup')
+            }
+        })
+})
+
+app.post('/api/auth/logout', (req, res) => {
+    res.clearCookie('loginToken')
+    res.send('logged-out!')
 })
 
 const port = 3030
